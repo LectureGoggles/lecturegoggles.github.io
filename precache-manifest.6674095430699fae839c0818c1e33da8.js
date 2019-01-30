a260b0f7102d2c3374d6adaf6937ce0f8f558e5f
Zack Johnson <zackjohnson8@gmail.com>
2019-01-30 14:59:37 -0800

    Updates

---
+++ precache-manifest.6674095430699fae839c0818c1e33da8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb5b8c82b5052913aa91f7108b05705",
+    "url": "/static/media/Kristian.4bb5b8c8.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49604e250baac9489f40",
+    "url": "/static/js/main.49604e25.chunk.js"
+  },
+  {
+    "revision": "5871c69d9200d6bcc116",
+    "url": "/static/js/1.5871c69d.chunk.js"
+  },
+  {
+    "revision": "efa0bb39b02331fe677a91e1d2e2bc14",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fb0b87fa60051826f6b0425dd3e19e5d.js"
+  "/precache-manifest.6674095430699fae839c0818c1e33da8.js"
 );
 
 workbox.clientsClaim();
