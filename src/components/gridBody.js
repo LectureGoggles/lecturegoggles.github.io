146e1030301d550ea664c660c7645fa200a0bc01
Nathan Yocum <nayocum@nevada.unr.edu>
2019-01-31 10:46:42 -0800

    Changed the description text to be more general, and made it more readable

--- src/components/gridBody.js
+++ src/components/gridBody.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
+// Should probably update when the window innerWidth changes, but for now it's fine
 const GridBody = styled.div`
   display: grid;
   grid-template-rows: 56px;
-  grid-template-columns: 1fr 4fr 1fr;
+  grid-template-columns: ${() => (window.innerWidth > 768 ? '1fr 2fr 1fr' : '16px 1fr 16px')};
   justify-items: center;
   margin-bottom: 56px;
 `;
